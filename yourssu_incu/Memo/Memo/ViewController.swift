d1554ab38a70c029953c79fcafce62dfdbb8dbc7
kimsoomin <skno1382@soongsil.ac.kr>
2023-05-21 23:35:56 +0900

    memo 플젝 파일 생성

---
+++ yourssu_incu/Memo/Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 김수민 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
